a05dd90c77b4b7dbd4f7cb9e42eb9f3cfddbaaa8
KDLT <aguirrekenneth@gmail.com>
2018-10-04 20:34:25 +0800

    fixed caret position, found character width

--- src/app/components/Display.js
+++ src/app/components/Display.js
@@ -1,31 +1,40 @@
 import React from 'react';
 
 export default (props) => {
+  let initialCaretPosition = 90;
+  let caretPosition = 90+23; //23 units ang laktaw ng cursor per number
   return (
-    <svg id="calculator" xmlns = "http://www.w3.org/2000/svg" viewBox = "0 0 633.89 1303.5">
-      <svg x='0' y='0' viewBox='0 0 100 100'>
-          <g id='svg-caret'><rect id="display" className="display-screen" x="72.98" y="189" width="490" height="195" rx="8.73" ry="8.73" />
-            <rect class="caret-square" width="1" height="1" />
-            <rect class="caret-square" y="1.3" width="1" height="1" />
-            <rect class="caret-square" y="2.61" width="1" height="1" />
-            <rect class="caret-square" y="3.91" width="1" height="1" />
-            <rect class="caret-square" y="5.22" width="1" height="1" />
-            <rect class="caret-square" y="6.52" width="1" height="1" />
-            <rect class="caret-square" y="7.83" width="1" height="1" />
-            <rect class="caret-square" y="9.13" width="1" height="1" />
-            <rect class="caret-square" y="10.44" width="1" height="1" />
-          </g>
-      </svg>
-      <rect id="display" className="display-screen" x="72.98" y="189" width="490" height="195" rx="8.73" ry="8.73" />
-      <svg width="550">
-        <text id="problem-text" x="90" y="250" width="490" width="490" height="195" rx="8.73" ry="8.73">
-          {props.problemForm}
-        </text>
-        <text id="answer-text" x="90" y="370" width="490" width="490" height="195" rx="8.73" ry="8.73">
-          {props.answer}
-        </text>
-      </svg>
-    </svg>
+    <g id="display">
+      <rect id="screen" className="display-screen" x="72.98" y="189" width="490" height="195" rx="8.73" ry="8.73" />
+      <g id='svg-caret'>
+        {/* <rect className="caret-square" x={caretPosition} y="217" width="3" height="33" /> */}
+        <rect x={caretPosition} y='217' width="3" height="3" />
+        <rect x={caretPosition} y="220.75" width="3" height="3" />
+        <rect x={caretPosition} y="224.5" width="3" height="3" />
+        <rect x={caretPosition} y="228.25" width="3" height="3" />
+        <rect x={caretPosition} y="232" width="3" height="3" />
+        <rect x={caretPosition} y="235.75" width="3" height="3" />
+        <rect x={caretPosition} y="239.5" width="3" height="3" />
+        <rect x={caretPosition} y="243.25" width="3" height="3" />
+        <rect x={caretPosition} y="247" width="3" height="3" />
+
+        <rect x={caretPosition+3.75} y='217' width="3" height="3" />
+        <rect x={caretPosition+3.75} y="220.75" width="3" height="3" />
+        <rect x={caretPosition+3.75} y="224.5" width="3" height="3" />
+        <rect x={caretPosition+3.75} y="228.25" width="3" height="3" />
+        <rect x={caretPosition+3.75} y="232" width="3" height="3" />
+        <rect x={caretPosition+3.75} y="235.75" width="3" height="3" />
+        <rect x={caretPosition+3.75} y="239.5" width="3" height="3" />
+        <rect x={caretPosition+3.75} y="243.25" width="3" height="3" />
+        <rect x={caretPosition+3.75} y="247" width="3" height="3" />
+      </g>
+      <text id="problem-text" x="90" y="250" width="490" width="490" height="195" rx="8.73" ry="8.73">
+        {props.problemForm}
+      </text>
+      <text id="answer-text" x="90" y="370" width="490" width="490" height="195" rx="8.73" ry="8.73">
+        {props.answer}
+      </text>
+    </g>
   )
 }
 // x='94' y='217'
